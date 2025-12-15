ec76b071adfb6c10fa0460b907313512801f038b
Bernd Verhofstadt (imec) <verhof65@imec.be>
2025-12-15 09:12:29 +0100

    Add Umami analytics tracking

---
+++ docs/assets/js/umami.js
@@ -0,0 +1,7 @@
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cloud.umami.is/script.js';
+  script.defer = true;
+  script.setAttribute('data-website-id', '2256a4d0-1c62-4110-bceb-e832655dfdd5');
+  document.head.appendChild(script);
+})();
